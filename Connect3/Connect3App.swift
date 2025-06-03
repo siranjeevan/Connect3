1452d13e99bfedddfae4b6370e8043bfb572b872
siranjeevan <smartjeevith2k@gamil.com>
2025-06-03 16:37:48 +0530

    Initial Commit

---
+++ Connect3/Connect3App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect3App.swift
+//  Connect3
+//
+//  Created by Jeevith  on 03/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Connect3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Connect3
+//
+//  Created by Jeevith  on 03/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

973c541b20f9a8385e23a52302d588f964a847b9
siranjeevan <smartjeevith2k@gamil.com>
2025-06-04 13:32:43 +0530

    Jeevith

---
+++ Connect3/Jeevith.swift
@@ -0,0 +1,18 @@
+//
+//  Jeevith.swift
+//  Connect3
+//
+//  Created by Jeevith  on 04/06/25.
+//
+
+import SwiftUI
+
+struct Jeevith: View {
+    var body: some View {
+        Text("jeevith")
+    }
+}
+
+#Preview {
+    Jeevith()
+}
